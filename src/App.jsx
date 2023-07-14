a21ef34262430632717243957ad923dbf1e7b2cd
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2023-07-14 13:11:28 -0400

    Change App to a class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import CVForm from "./components/sections/CVForm";
+import { Component } from "react";
 
-function App() {
-  return <CVForm />;
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <CVForm />;
+  }
 }
 
 export default App;
